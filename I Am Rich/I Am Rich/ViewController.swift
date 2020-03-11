1378f642faa3518ab2c36850b5ce42be5fdd1a5a
Marc Lassort <marc.lassort@icloud.com>
2020-03-11 16:11:44 +0100

    Mes projets

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Marc Lassort on 05/03/2020.
+//  Copyright © 2020 Marc Lassort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Quizzler-iOS13/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var answerOne: UIButton!
+    @IBOutlet weak var answerTwo: UIButton!
+    @IBOutlet weak var answerThree: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+        
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+            score += 1
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+    }
+    
+    @objc func updateUI() {
+                
+        answerOne.setTitle(quizBrain.getAnswerText()[0], for: .normal)
+        answerTwo.setTitle(quizBrain.getAnswerText()[1], for: .normal)
+        answerThree.setTitle(quizBrain.getAnswerText()[2], for: .normal)
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        answerOne.backgroundColor = UIColor.clear
+        answerTwo.backgroundColor = UIColor.clear
+        answerThree.backgroundColor = UIColor.clear
+        
+    }
+    
+    
+}
+
+
+
